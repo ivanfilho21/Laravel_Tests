a2d4608bfb103a2683d09717c53c333fe2e2b4ec
ivanfilho21 <ivanfilho21@gmail.com>
2019-08-28 15:06:12 -0300

    Selecting products from db

--- stock/app/Http/Controllers/ProductController.php
+++ stock/app/Http/Controllers/ProductController.php
@@ -2,10 +2,18 @@
 
 namespace stock\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
+
 class ProductController
 {
     public function list()
     {
-        return "<h1>Products</h1>";
+        $products = DB::select("select * from produtos");
+        $ul = "<ul>";
+        foreach ($products as $p) {
+            $ul .= "<li>" .$p->nome ." " .$p->descricao ."</li>";
+        }
+        $ul .= "</ul>";
+        return "<h1>Products</h1>" .$ul;
     }
 }
