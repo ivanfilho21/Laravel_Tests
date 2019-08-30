b492e3c23dc6a0800bc784ad7ff4ec9ed3ea522a
Ivan Filho <ivanfilho21@gmail.com>
2019-08-30 12:49:41 -0300

    Updates

--- stock/app/Http/Controllers/ProductController.php
+++ stock/app/Http/Controllers/ProductController.php
@@ -3,14 +3,35 @@
 namespace stock\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
+use Request;
 
 class ProductController
 {
     public function list()
     {
-        $products = DB::select("select * from produtos");
+        $products = DB::select("SELECT * FROM produtos");
 
-        // return view("products")->withProducts($products);
-        return (view()->exists("products")) ? view("products")->with("products", $products) : "Page Not Found";
+        $data = array(
+        	"products" => $products
+        );
+        return $this->getView("products", $data);
+    }
+
+    public function open()
+    {
+    	$id = Request::input("id", 0);
+    	$product = DB::select("SELECT * FROM produtos WHERE id = :id", array(":id" => $id));
+
+    	if (empty($id)) return "Produto não existe";
+
+    	$data = array(
+    		"p" => $product[0]
+    	);
+    	return $this->getView("product", $data);
+    }
+
+    private function getView($viewName, $viewData = array())
+    {
+    	return (view()->exists($viewName)) ? view($viewName, $viewData) : "Page Not Found";
     }
 }
--- stock/resources/views/products.php
+++ stock/resources/views/products.php
@@ -5,16 +5,34 @@
 	<link href="/css/app.css" rel="stylesheet">
 </head>
 <body>
+	<nav>
+		<ul>
+			<li><a href="#">List</a></li>
+			<li><a href="#">New Product</a></li>
+		</ul>
+	</nav>
 	<div class="container">
-		<h1>Products</h1>
+		<h1>Produtos</h1>
 
 		<table class="table table-striped table-bordered table-hover">
 		<?php foreach ($products as $p): ?>
 			<tr>
 				<td><?= $p->nome ?></td>
-				<td><?= $p->valor ?></td>
+				<td>R$ <?= $p->valor ?></td>
 				<td><?= $p->descricao ?></td>
 				<td><?= $p->quantidade ?></td>
+
+				<td>
+					<a href="produto?id=<?= $p->id ?>" class="btn btn-default">
+						<span class="glyphicon glyphicon-search"></span>
+					</a>
+					<a href="produto/editar?id=<?= $p->id ?>" class="btn btn-default">
+						<span class="glyphicon glyphicon-pencil"></span>
+					</a>
+					<a href="produto/apagar?id=<?= $p->id ?>" class="btn btn-default">
+						<span class="glyphicon glyphicon-trash"></span>
+					</a>
+				</td>
 			</tr>
 		<?php endforeach ?>
 		</table>
