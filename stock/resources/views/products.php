657417906a9f788f72960ffbc2e00aef0d5e3d83
Ivan Filho <ivanfilho21@gmail.com>
2019-08-30 11:43:23 -0300

    Added styles

--- stock/resources/views/products.php
+++ stock/resources/views/products.php
@@ -2,19 +2,22 @@
 <html>
 <head>
 	<title></title>
+	<link href="/css/app.css" rel="stylesheet">
 </head>
 <body>
-	<h1>Products</h1>
+	<div class="container">
+		<h1>Products</h1>
 
-	<table>
-	<?php foreach ($products as $p): ?>
-		<tr>
-			<td><?= $p->nome ?></td>
-			<td><?= $p->valor ?></td>
-			<td><?= $p->descricao ?></td>
-			<td><?= $p->quantidade ?></td>
-		</tr>
-	<?php endforeach ?>
-	</table>
+		<table class="table table-striped table-bordered table-hover">
+		<?php foreach ($products as $p): ?>
+			<tr>
+				<td><?= $p->nome ?></td>
+				<td><?= $p->valor ?></td>
+				<td><?= $p->descricao ?></td>
+				<td><?= $p->quantidade ?></td>
+			</tr>
+		<?php endforeach ?>
+		</table>
+	</div>
 </body>
 </html>
