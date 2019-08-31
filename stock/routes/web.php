e3a7df31e01accca75f3c8e273f499c7aa9063c0
Ivan Filho <ivanfilho21@gmail.com>
2019-08-31 18:40:22 -0300

    Add routes

--- stock/routes/web.php
+++ stock/routes/web.php
@@ -3,6 +3,9 @@
 Route::get("/", function() { return "Hello World"; });
 
 Route::get("/produtos", "ProductController@list");
+Route::get("/produtos/json", "ProductController@listJson");
+Route::get("/produto", "ProductController@create");
 Route::get("/produto/{id}", "ProductController@open")->where("id", "[0-9]+");
+Route::post("/produtos/add", "ProductController@add");
 /*Route::get("/produto/editar/{id}", "ProductController@edit");
-Route::get("/produto/apagar/{id}", "ProductController@delete");*/
+Route::get("/produto/apagar/{id}", "ProductController@delete");
