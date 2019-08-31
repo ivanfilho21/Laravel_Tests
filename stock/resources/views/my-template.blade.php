f943b420316bcb8d93c4b88900958f105bfa9ae2
Ivan Filho <ivanfilho21@gmail.com>
2019-08-31 12:11:18 -0300

    Create blade tamplate

---
+++ stock/resources/views/my-template.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Estoque</title>
+	<link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+	<nav>
+		<ul>
+			<li><a href="#">List</a></li>
+			<li><a href="#">New Product</a></li>
+		</ul>
+	</nav>
+	<div class="container">
+		@yield("content")
+	</div>
+</body>
+</html>
