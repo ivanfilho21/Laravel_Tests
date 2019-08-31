8d697f809dbade9b21ed09d360aef73c7c9ad070
Ivan Filho <ivanfilho21@gmail.com>
2019-08-31 12:11:47 -0300

    Modify route to product page

--- stock/routes/web.php
+++ stock/routes/web.php
@@ -3,6 +3,6 @@
 Route::get("/", function() { return "Hello World"; });
 
 Route::get("/produtos", "ProductController@list");
-Route::get("/produto", "ProductController@open");
+Route::get("/produto/{id}", "ProductController@open")->where("id", "[0-9]+");
 /*Route::get("/produto/editar/{id}", "ProductController@edit");
 Route::get("/produto/apagar/{id}", "ProductController@delete");*/
