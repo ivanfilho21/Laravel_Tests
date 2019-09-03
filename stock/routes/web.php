75e7cf4fee9c0ac647a3f81dd337c3283279d6e8
Ivan Filho <ivanfilho21@gmail.com>
2019-09-03 11:42:45 -0300

    Update routes

--- stock/routes/web.php
+++ stock/routes/web.php
@@ -4,8 +4,8 @@ Route::get("/", function() { return "Hello World"; });
 
 Route::get("/produtos", "ProductController@list");
 Route::get("/produtos/json", "ProductController@listJson");
-Route::get("/produto", "ProductController@create");
 Route::get("/produto/{id}", "ProductController@open")->where("id", "[0-9]+");
-Route::post("/produtos/add", "ProductController@add");
-Route::get("/produto/editar/{id}", "ProductController@edit");
-Route::get("/produto/apagar/{id}", "ProductController@delete");
+Route::post("/produtos/save", "ProductController@save");
+Route::get("/produto", "ProductController@createEdit");
+Route::get("/produto/editar/{id?}", "ProductController@createEdit")->where("id", "[0-9]+");
+Route::get("/produto/apagar/{id}", "ProductController@delete")->where("id", "[0-9]+");
