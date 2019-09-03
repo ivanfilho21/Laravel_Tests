ad552893e59ab3afcb9da04cdd83a9ed9fd3c5ff
Ivan Filho <ivanfilho21@gmail.com>
2019-09-03 11:44:44 -0300

    Show error messages. Add old fields. Add hidden input for id.

--- stock/resources/views/product/product-form.blade.php
+++ stock/resources/views/product/product-form.blade.php
@@ -5,33 +5,46 @@
 <h1>Novo Produto</h1>
 <br>
 
-<form method="post" action="/produtos/add">
+@if (count($errors) > 0)
+<div class="alert alert-danger">
+	<h5>Foram encontrados os seguintes erros:</h5>
+	<ul>
+		@foreach ($errors->all() as $e)
+		<li>{{ $e }}</li>
+		@endforeach	
+	</ul>
+</div>
+@endif
+
+
+<form method="post" action="/produtos/save">
 	<input type="hidden" name="_token" value="{{{ csrf_token() }}}">
+	<input type="hidden" name="id" value="{{ ! empty($p) ? $p->id : 0 }}">
 
 	<div class="form-group">
 		<label>Nome:</label>
-		<input class="form-control" type="text" name="name">
+		<input class="form-control" type="text" name="name" value="{{ old('name') ? old('name') : (! empty($p) ? $p->nome : '') }}">
 	</div>
 
 	<div class="form-group">
 		<label>Descrição:</label>
-		<input class="form-control" type="text" name="description">
+		<input class="form-control" type="text" name="description" value="{{ old('description') ? old('description') : (! empty($p) ? $p->descricao : '') }}">
 	</div>
 
 	<div class="form-group">
 		<label>Valor:</label>
-		<input class="form-control" type="text" name="price">
+		<input class="form-control" type="text" name="price" value="{{ old('price') ? old('price') : (! empty($p) ? $p->valor : '') }}">
 	</div>
 
 	<div class="form-group">
 		<label>Quantidade:</label>
-		<input class="form-control" type="text" name="qty">
+		<input class="form-control" type="text" name="qty" value="{{ old('qty') ? old('qty') : (! empty($p) ? $p->quantidade : '') }}">
 	</div>
 
 	<br>
 	<div class="row">
 	    <div class="col-md-2 col-centered text-center">
-			<button class="btn btn-success" type="submit">Adicionar Produto</button>
+			<button class="btn btn-success" type="submit">{{ ! empty($p) ? "Editar" : "Adicionar" }} Produto</button>
 	    </div>
 	</div>
 </form>
