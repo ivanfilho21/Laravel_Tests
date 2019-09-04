3368598ad5b03066faedc7649e5413d90f35e5d6
Ivan Filho <ivanfilho21@gmail.com>
2019-09-04 12:06:26 -0300

    Add routes

--- stock/routes/web.php
+++ stock/routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
 // Route::get("/", function() { return "Hello World"; });
-// Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 Route::get("/", "HomeController@index")->name("home");
+Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get("/produtos", "ProductController@list");
 Route::get("/produtos/json", "ProductController@listJson");
@@ -12,3 +12,5 @@ Route::post("/produtos/save", "ProductController@save");
 Route::get("/produto", "ProductController@createEdit");
 Route::get("/produto/editar/{id?}", "ProductController@createEdit")->where("id", "[0-9]+");
 Route::get("/produto/apagar/{id}", "ProductController@delete")->where("id", "[0-9]+");
+
+Route::get("/mylogin", "MyAuthController@login");
