dd521bf189fb0a0b834cc656c1016b6661c54a5a
Ivan Filho <ivanfilho21@gmail.com>
2020-08-17 16:31:53 -0300

    Fix mispelled table name

--- crudAuth/dump-mysql.sql
+++ crudAuth/dump-mysql.sql
@@ -1,1 +1,1 @@
-create table users(id bigint unsigned not null auto_increment primary key, nome varchar(255), data_nascimento datetime not null, criado_em timestamp null, atualizado_em timestamp null);
+create table pets(id bigint unsigned not null auto_increment primary key, nome varchar(255), data_nascimento datetime not null, criado_em timestamp null, atualizado_em timestamp null);
