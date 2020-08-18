7707ef4852c6799ef76579e4311f04112fa4c944
Ivan Filho <ivanfilho21@gmail.com>
2020-08-18 10:23:08 -0300

    Create panel homepage

---
+++ cms/resources/views/admin_panel/home.blade.php
@@ -0,0 +1,16 @@
+@extends('adminlte::page')
+@section('title', 'Painel')
+
+@section('css')
+@endsection
+
+@section('content_header')
+Header
+@endsection
+
+@section('content')
+Conteudo
+@endsection
+
+@section('js')
+@endsection
