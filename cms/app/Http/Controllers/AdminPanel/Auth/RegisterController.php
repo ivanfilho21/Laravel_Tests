6418c7554e9efaf0dc881d84075e740c8480e309
Ivan Filho <ivanfilho21@gmail.com>
2020-08-24 12:05:44 -0300

    Implement registration

--- cms/app/Http/Controllers/AdminPanel/Auth/RegisterController.php
+++ cms/app/Http/Controllers/AdminPanel/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\Providers\RouteServiceProvider;
 use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 use Illuminate\Http\Request;
@@ -30,15 +31,29 @@ class RegisterController extends Controller
 
     public function register(Request $request)
     {
-        #
+        $dados = $request->only([
+            'name', 'email', 'password', 'password_confirmation'
+        ]);
+
+        $validator = $this->validator($dados);
+        if ($validator->fails()) {
+            return redirect()->route('panel.register')
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        $user = $this->create($dados);
+        Auth::login($user);
+
+        return redirect()->route('panel.index');
     }
 
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'name' => ['required', 'string', 'max:100'],
+            'email' => ['required', 'string', 'email', 'max:200', 'unique:users'],
+            'password' => ['required', 'string', 'min:6', 'confirmed'],
         ]);
     }
 
