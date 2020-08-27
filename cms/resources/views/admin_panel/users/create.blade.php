076568794563ebf93fcb26b9088f41567b9f96f4
Ivan Filho <ivanfilho21@gmail.com>
2020-08-27 10:37:22 -0300

    Update view

--- cms/resources/views/admin_panel/users/create.blade.php
+++ cms/resources/views/admin_panel/users/create.blade.php
@@ -6,5 +6,53 @@
 @endsection
 
 @section('content')
-...
+
+<form action="{{ route('users.store') }}" method="post" class="form-horizontal">
+    @csrf
+
+    <div class="form-group">
+        <div class="row">
+            <label for="" class="col-sm-2 control-label">{{ __('attribs.name') }}:</label>
+            <div class="col-sm-10">
+                <input type="text" name="name" class="form-control">
+            </div>
+        </div>
+    </div>
+
+    <div class="form-group">
+        <div class="row">
+            <label for="" class="col-sm-2 control-label">{{ __('attribs.email') }}:</label>
+            <div class="col-sm-10">
+                <input type="email" name="email" class="form-control">
+            </div>
+        </div>
+    </div>
+
+    <div class="form-group">
+        <div class="row">
+            <label for="" class="col-sm-2 control-label">{{ __('attribs.password') }}:</label>
+            <div class="col-sm-10">
+                <input type="password" name="password" class="form-control">
+            </div>
+        </div>
+    </div>
+
+    <div class="form-group">
+        <div class="row">
+            <label for="" class="col-sm-2 control-label">{{ __('attribs.password_confirmation') }}:</label>
+            <div class="col-sm-10">
+                <input type="password" name="password_confirmation" class="form-control">
+            </div>
+        </div>
+    </div>
+
+    <div class="form-group">
+        <div class="row">
+            <label for="" class="col-sm-2 control-label"></label>
+            <div class="col-sm-10">
+                <input type="submit" value="{{ __('util.create') }}" class="btn btn-success">
+            </div>
+        </div>
+    </div>
+</form>
 @endsection
