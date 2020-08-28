1a5a9fdda2579b378e7965183836b3cf86f12e52
Ivan Filho <ivanfilho21@gmail.com>
2020-08-28 16:57:01 -0300

    Add pagination and card

--- cms/resources/views/admin_panel/users/index.blade.php
+++ cms/resources/views/admin_panel/users/index.blade.php
@@ -6,29 +6,34 @@
 @endsection
 
 @section('content')
-<table class="table table-hover">
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>{{ __('attribs.name') }}</th>
-            <th>{{ __('attribs.email') }}</th>
-            <th>{{ __('util.actions') }}</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($users as $u)
-        <tr>
-            <td>{{ $u->id }}</td>
-            <td>{{ $u->name }}</td>
-            <td>{{ $u->email }}</td>
-            <td>
-                <a class="btn btn-sm btn-info" href="{{ route('users.edit', ['user' => $u->id]) }}">{{ __('util.edit') }}</a>
-                <a class="btn btn-sm btn-danger" href="{{ route('users.destroy', ['user' => $u->id]) }}">{{ __('util.delete') }}</a>
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-    
-    <a class="btn btn-primary" href="{{ route('users.create') }}" style="width: 56px; height: 56px; line-height: 42px; margin: 1em; border-radius: 50%; position: absolute; bottom: 0; right: 0;"><span class="fas fa-plus"></span></a>
-</table>
+<div class="card">
+    <div class="card-body">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>{{ __('attribs.name') }}</th>
+                    <th>{{ __('attribs.email') }}</th>
+                    <th>{{ __('util.actions') }}</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($users as $u)
+                <tr>
+                    <td>{{ $u->id }}</td>
+                    <td>{{ $u->name }}</td>
+                    <td>{{ $u->email }}</td>
+                    <td>
+                        <a class="btn btn-sm btn-info" href="{{ route('users.edit', ['user' => $u->id]) }}">{{ __('util.edit') }}</a>
+                        <a class="btn btn-sm btn-danger" href="{{ route('users.destroy', ['user' => $u->id]) }}">{{ __('util.delete') }}</a>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+            
+        </table>
+    </div>
+    {{ $users->links() }}
+</div>
+<a class="btn btn-primary" href="{{ route('users.create') }}" style="width: 56px; height: 56px; line-height: 42px; margin: 1em; border-radius: 50%; position: absolute; bottom: 0; right: 0;"><span class="fas fa-plus"></span></a>
 @endsection
