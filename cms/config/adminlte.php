65ada54878eb4116682d45a6f0067734d4d14b4e
Ivan Filho <ivanfilho21@gmail.com>
2020-08-29 17:40:13 -0300

    Update menu

--- cms/config/adminlte.php
+++ cms/config/adminlte.php
@@ -231,6 +231,7 @@ return [
     */
 
     'menu' => [
+        ['header' => 'main_navigation'],
         /* [
             'text' => 'search',
             'search' => true,
@@ -244,80 +245,32 @@ return [
         [
             'text'        => 'Dashboard',
             'url'         => 'panel',
-            'icon'        => 'far fa-fw fa-file',
+            'icon'        => 'fas fa-fw fa-chart-bar',
             'label'       => null,
             'label_color' => 'success',
         ],
         [
             'text' => 'pages',
             'url'  => 'panel/pages',
-            'icon' => 'fas fa-fw fa-file',
+            'icon' => 'far fa-fw fa-copy',
         ],
         [
             'text' => 'users',
             'url'  => 'panel/users',
-            'icon' => 'fas fa-fw fa-users',
+            'icon' => 'far fa-fw fa-users',
+            'can'  => 'edit-users',
         ],
         ['header' => 'account_settings'],
         [
             'text' => 'profile',
             'url'  => 'panel/profile',
-            'icon' => 'fas fa-fw fa-user',
-        ],
-        /* [
-            'text'    => 'multilevel',
-            'icon'    => 'fas fa-fw fa-share',
-            'submenu' => [
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-                [
-                    'text'    => 'level_one',
-                    'url'     => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'level_two',
-                            'url'  => '#',
-                        ],
-                        [
-                            'text'    => 'level_two',
-                            'url'     => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-            ],
-        ], */
-        /* ['header' => 'labels'],
-        [
-            'text'       => 'important',
-            'icon_color' => 'red',
-            'url'        => '#',
+            'icon' => 'far fa-fw fa-user-circle',
         ],
         [
-            'text'       => 'warning',
-            'icon_color' => 'yellow',
-            'url'        => '#',
-        ],
-        [
-            'text'       => 'information',
-            'icon_color' => 'cyan',
-            'url'        => '#',
-        ], */
+            'text' => 'settings',
+            'url'  => 'panel/settings',
+            'icon' => 'far fa-fw fa-sun'
+        ]
     ],
 
     /*
