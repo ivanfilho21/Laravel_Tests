76faf6d09bafc00d3dc489a2db30f148b7c5228c
Ivan Filho <ivanfilho21@gmail.com>
2020-08-31 11:24:17 -0300

    Update picture path

--- cms/resources/views/admin_panel/profile/index.blade.php
+++ cms/resources/views/admin_panel/profile/index.blade.php
@@ -6,24 +6,49 @@
 @endsection
 
 @section('content')
-<div class="card">
 
-    <div class="card-body">
-        <div class="row">
-            <form action="{{ route('panel.profile.save') }}" method="post">
-                @csrf
-                @method('PUT')
+@if (session('success'))
+<div class="alert alert-success alert-dismissable">
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+    <h5><i class="icon fas fa-check"></i>{{ __('util.alert') }}</h5>
+    {{ session('success') }}
+</div>
+@endif
 
-                <label for="">{{ __('attribs.name') }}</label>
-                <input type="text" name="" id="" value="{{ old('name', $user->name) }}">
+<form action="{{ route('panel.profile.save') }}" method="post">
+    @csrf
+    @method('PUT')
 
-                <input type="submit" class="btn btn-success" value="{{ __('util.save') }}">
-            </form>
+    <div class="card">
+        <div class="card-body">
+            <div class="image text-center">
+                <img src="{{ asset('profile.jpg') }}" alt="Profile Picture" class="img-circle">
+            </div>
+
+            <div class="form-group">
+                <label for="" class="col-label">{{ __('attribs.name') }}:</label>
+                <input type="text" name="name" id="" value="{{ old('name', $user->name) }}" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label for="" class="col-label">{{ __('attribs.email') }}:</label>
+                <input type="email" name="email" disabled id="" value="{{ old('email', $user->email) }}" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label for="" class="col-label">{{ __('attribs.password') }}:</label>
+                <input type="password" name="password" id="" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label for="" class="col-label">{{ __('attribs.password_confirmation') }}:</label>
+                <input type="password" name="password_confirmation" id="" class="form-control">
+            </div>
         </div>
 
-        <div class="row">
-            Email
+        <div class="card-footer text-center">
+            <button type="submit" class="btn btn-success">{{ __('util.update_profile') }}</button>
         </div>
     </div>
-</div>
+</form>
 @endsection
