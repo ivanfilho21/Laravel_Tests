22e24adda7e6a2b65612b0c99188fc1315a72144
Ivan Filho <ivanfilho21@gmail.com>
2020-09-01 16:10:21 -0300

    Add value

--- cms/resources/lang/en/util.php
+++ cms/resources/lang/en/util.php
@@ -11,5 +11,6 @@ return [
     'alert' => 'Alert',
     'update_profile' => 'Update Profile',
     'users_update_success' => 'The user was successfully updated.',
+    'site_settings_update_success' => 'Site settings were succesfully updated.',
     'confirm_delete_users' => 'Delete the user ":user"?',
 ];
