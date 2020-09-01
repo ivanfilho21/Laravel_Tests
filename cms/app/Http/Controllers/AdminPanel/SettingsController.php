f42bfb4a4a9e56fdfa0d8cbcc63066550836490b
Ivan Filho <ivanfilho21@gmail.com>
2020-09-01 16:10:55 -0300

    Implement save settings method

--- cms/app/Http/Controllers/AdminPanel/SettingsController.php
+++ cms/app/Http/Controllers/AdminPanel/SettingsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\AdminPanel;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use App\Http\Controllers\Controller;
 use App\Settings;
 
@@ -24,6 +25,35 @@ class SettingsController extends Controller
         }
 
         return view('admin_panel.settings.index', ['settings' => $settings]);
+    }
+
+    public function save(Request $request)
+    {
+        $data = $request->only([
+            'title', 'subtitle', 'bg_color', 'pri_txt_color'
+        ]);
+
+        $regHex = '/#[a-zA-Z0-9]{6}/i';
+        $validator = Validator::make($data, [
+            'title' => ['required', 'string', 'max:100'],
+            'subtitle' => ['string', 'nullable', 'max:100'],
+            'bg_color' => ['string', 'regex:'.$regHex],
+            'pri_txt_color' => ['string', 'regex:'.$regHex],
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->back()
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        foreach ($data as $key => $value) {
+            Settings::where('name', $key)->update([
+                'value' => $value
+            ]);
+        }
+
+        return redirect()->back()->with('success', __('util.site_settings_update_success'));
     }
 
 }
