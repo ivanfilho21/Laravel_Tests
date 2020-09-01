10ed5e3e5c97aca5357850b7653a27b91f68534c
Ivan Filho <ivanfilho21@gmail.com>
2020-09-01 16:10:37 -0300

    Update form

--- cms/resources/views/admin_panel/settings/index.blade.php
+++ cms/resources/views/admin_panel/settings/index.blade.php
@@ -7,6 +7,14 @@
 @endsection
 
 @section('content')
+    @if (session('success'))
+    <div class="alert alert-success alert-dismissable">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <h5><i class="icon fas fa-check"></i>{{ __('util.alert') }}</h5>
+        {{ session('success') }}
+    </div>
+    @endif
+
     <form action="{{ route('panel.settings.save') }}" method="post">
         @csrf
         @method('PUT')
@@ -15,27 +23,39 @@
             <div class="card-body">
                 <div class="form-group">
                     <label for="" class="col-form-label">{{ __('site_settings.title') }}:</label>
-                    <input type="text" name="title" id="" value="{{ old('title', $settings['title']) }}" class="form-control">
+                    <input type="text" name="title" id="" value="{{ old('title', $settings['title']) }}" class="form-control @error('title') is-invalid @enderror">
+                    @error('title')
+                        <span class="text-danger">{{ $message }}</span>
+                    @enderror
                 </div>
 
                 <div class="form-group">
                     <label for="" class="col-form-label">{{ __('site_settings.subtitle') }}:</label>
-                    <input type="text" name="subtitle" id="" value="{{ old('subtitle', $settings['subtitle']) }}" class="form-control">
+                    <input type="text" name="subtitle" id="" value="{{ old('subtitle', $settings['subtitle']) }}" class="form-control @error('subtitle') is-invalid @enderror">
+                    @error('subtitle')
+                        <span class="text-danger">{{ $message }}</span>
+                    @enderror
                 </div>
 
                 <div class="form-group">
                     <label for="" class="col-form-label">{{ __('site_settings.bg_color') }}:</label>
                     <input type="color" name="bg_color" id="" value="{{ old('bg_color', $settings['bg_color']) }}" class="form-control" style="width: 50px;">
+                    @error('bg_color')
+                        <span class="text-danger">{{ $message }}</span>
+                    @enderror
                 </div>
 
                 <div class="form-group">
                     <label for="" class="col-form-label">{{ __('site_settings.pri_txt_color') }}:</label>
                     <input type="color" name="pri_txt_color" id="" value="{{ old('pri_txt_color', $settings['pri_txt_color']) }}" class="form-control" style="width: 50px;">
+                    @error('pri_txt_color')
+                        <span class="text-danger">{{ $message }}</span>
+                    @enderror
                 </div>
             </div>
 
             <div class="card-footer text-center">
-                <input type="submit" value="{{ __('util.save') }}" class="btn btn-success">
+                <button type="submit" class="btn btn-success">{{ __('util.save') }}</button>
             </div>
         </div>
     </form>
