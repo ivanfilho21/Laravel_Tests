a605f4b0b14ffcab675bff4d6e27d392b37aa081
Ivan Filho <ivanfilho21@gmail.com>
2020-09-07 17:20:50 -0300

    Add fillable property

--- cms/app/Page.php
+++ cms/app/Page.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Page extends Model
 {
-    //
+    
+    protected $fillable = [
+        'title', 'body', 'slug', 'created_by', 'updated_by'
+    ];
 }
