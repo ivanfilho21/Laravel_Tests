b4f7bd3415ef495042d180919eb8d3e2c51c4e07
Ivan Filho <ivanfilho21@gmail.com>
2020-09-07 17:39:34 -0300

    Remove comment

--- cms/app/Http/Controllers/AdminPanel/PageController.php
+++ cms/app/Http/Controllers/AdminPanel/PageController.php
@@ -89,12 +89,6 @@ class PageController extends Controller
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         $page = Page::find($id);
