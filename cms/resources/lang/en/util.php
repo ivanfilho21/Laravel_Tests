2160a4eb571280c4b35786cc39f59f293874da32
Ivan Filho <ivanfilho21@gmail.com>
2020-09-09 17:04:55 -0300

    Update lang

--- cms/resources/lang/en/util.php
+++ cms/resources/lang/en/util.php
@@ -11,6 +11,7 @@ return [
     'alert' => 'Alert',
     'update_profile' => 'Update Profile',
     'users_update_success' => 'The user was successfully updated.',
+    'pages_update_success' => 'The page was successfully updated.',
     'layout_update_success' => 'The layout was succesfully updated.',
     'confirm_delete_pages' => 'Delete the page ":page"?',
     'confirm_delete_users' => 'Delete the user ":user"?',
