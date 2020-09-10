71833a50b859f259bdd6b3c660c92ec9cdde80bb
Ivan Filho <ivanfilho21@gmail.com>
2020-09-10 16:40:09 -0300

    Change title

--- cms/resources/views/admin_panel/home.blade.php
+++ cms/resources/views/admin_panel/home.blade.php
@@ -1,11 +1,11 @@
 @extends('adminlte::page')
-@section('title', __('titles.panel'))
+@section('title', __('titles.dashboard'))
 
 @section('css')
 @endsection
 
 @section('content_header')
-Header
+<h1>{{ __('titles.dashboard') }}</h1>
 @endsection
 
 @section('content')
