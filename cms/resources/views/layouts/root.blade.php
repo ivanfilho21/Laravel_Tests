1dcdb8d3293963fa39f303fb7435667c78d914af
Ivan Filho <ivanfilho21@gmail.com>
2020-09-17 17:03:28 -0300

    Update root template

--- cms/resources/views/layouts/root.blade.php
+++ cms/resources/views/layouts/root.blade.php
@@ -2,20 +2,20 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ ''/* $pageTitle */ }} | {{ config('app.name', 'Laravel') }}</title>
+    <title>@yield('title') | {{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    @stack('head-scripts')
+    @stack('head_scripts')
 
     <!-- Styles -->
-    @stack('head-styles')
+    @stack('head_styles')
 </head>
 <body>
-    @yield('body-content')
+    @yield('body_content')
     
     <!-- Scripts -->
-    @yield('body-scripts')
+    @yield('body_scripts')
 </body>
 </html>
