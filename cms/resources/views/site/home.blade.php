df63af3abc49264f8a30ef7afbdaacdeb3e5b429
Ivan Filho <ivanfilho21@gmail.com>
2020-09-18 18:38:32 -0300

    Show menus in site home page

--- cms/resources/views/site/home.blade.php
+++ cms/resources/views/site/home.blade.php
@@ -20,33 +20,22 @@
 
         <div class="collapse navbar-collapse" id="navbarsExampleDefault">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link page-scroll" href="#header">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link page-scroll" href="#services">Services</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link page-scroll" href="#pricing">Pricing</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link page-scroll" href="#request">Request</a>
-                </li>
-
-                <!-- Dropdown Menu -->          
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle page-scroll" href="#about" id="navbarDropdown" role="button" aria-haspopup="true" aria-expanded="false">About</a>
+            @foreach ($menus as $menu)
+                <li class="nav-item @if ($menu->submenus) dropdown @endif">
+                    <a class="nav-link @if ($menu->submenus) dropdown-toggle @endif page-scroll" href="{{ $menu->page->slug }}">{{ $menu->page->title }}</a>
+                    @if ($menu->submenus)
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="terms-conditions.html"><span class="item-text">Terms Conditions</span></a>
+                        @for ($i = 0; $i < count($menu->submenus); $i++)
+                        @php $sub = $menu->submenus[$i]->page @endphp
+                        <a class="dropdown-item" href="{{ $sub->slug }}"><span class="item-text">{{ $sub->title }}</span></a>
+                        @if ($i < count($menu->submenus) -1)
                         <div class="dropdown-items-divide-hr"></div>
-                        <a class="dropdown-item" href="privacy-policy.html"><span class="item-text">Privacy Policy</span></a>
+                        @endif
+                        @endfor
                     </div>
+                    @endif
                 </li>
-                <!-- end of dropdown menu -->
-
-                <li class="nav-item">
-                    <a class="nav-link page-scroll" href="#contact">Contact</a>
-                </li>
+            @endforeach
             </ul>
             <span class="nav-item social-icons">
                 <span class="fa-stack">
