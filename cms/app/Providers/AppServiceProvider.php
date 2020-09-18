70367defe9ee7a7d7120bf14dbd7da82200cbfa6
Ivan Filho <ivanfilho21@gmail.com>
2020-09-18 18:38:51 -0300

    Share menus to views

--- cms/app/Providers/AppServiceProvider.php
+++ cms/app/Providers/AppServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+
+use App\Page;
+use App\Menu;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +27,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Compartilha os menus do front com todas as views
+        $menusAll = Menu::all();
+        $menus = [];
+
+        foreach ($menusAll as $menu) {
+            $parentMenu = Menu::find($menu->parent_id);
+            if ($parentMenu) continue;
+
+            $menus[] = $menu;
+        }
+
+        View::share('menus', $menus);
     }
 }
