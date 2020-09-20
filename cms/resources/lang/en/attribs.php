740b82a12c168962b6b2a4225900ccb178ea435d
Ivan Filho <ivanfilho21@gmail.com>
2020-09-20 11:56:14 -0300

    Update lang

--- cms/resources/lang/en/attribs.php
+++ cms/resources/lang/en/attribs.php
@@ -8,6 +8,9 @@ return [
     'password_confirmation' => 'Confirm the password',
     'title' => 'Title',
     'body' => 'Body',
+    'page' => 'Page',
+    'from_website' => 'From site',
+    'external' => 'External',
 
     'submenu' => 'Submenu',
     
