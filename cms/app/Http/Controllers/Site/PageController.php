60d8a43b041635b59c07136febc0accfa3992928
Ivan Filho <ivanfilho21@gmail.com>
2020-09-22 09:33:49 -0300

    Create controller to handler pages in site

---
+++ cms/app/Http/Controllers/Site/PageController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Site;
+
+use Illuminate\Http\Request;
+
+use App\Http\Controllers\Controller;
+use App\Page;
+
+class PageController extends Controller
+{
+    
+    public function index($slug)
+    {
+        $page = Page::where('slug', $slug)->first();
+
+        if (! $page) {
+            abort(404);
+        }
+
+        return view('site.page', ['page' => $page]);
+    }
+}
