66d876b93d45cc2838166288a5afd778ccb8d296
Ivan Filho <ivanfilho21@gmail.com>
2020-09-22 09:20:51 -0300

    Add site title in template

--- cms/resources/views/layouts/root.blade.php
+++ cms/resources/views/layouts/root.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title') | {{ config('app.name', 'Laravel') }}</title>
+    <title>@yield('title'){{ isset($siteLayout['title']) ? " | {$siteLayout['title']}" : '' }}</title>
 
     <!-- Scripts -->
     @stack('head_scripts')
